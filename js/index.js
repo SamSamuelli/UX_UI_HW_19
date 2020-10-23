a0132f5f3ec06b06a9872af2920e0748e09ef1ec
SamSamuelli <samuelli.samantha@gmail.com>
2020-10-22 18:22:19 -0600

    Adding .active class

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function scrollNav() {
+    $(".menu a").click(function(){
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+    
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top - 160
+    }, 300);
+    return false;
+    });
+}
+scrollNav();
